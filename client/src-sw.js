9a7e6121131549a32e463b3b896cfd534032fcd3
unknown <joseph.harley02@gmail.com>
2023-03-02 18:30:47 -0500

    fixed syntax error in database.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,7 +27,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// Implements asset caching
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,21 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// accecpts content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ content: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// gets all the content from the database
 export const getDb = async () => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   return result;
 };
